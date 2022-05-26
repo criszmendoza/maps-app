8812811958d5d8070372cb807542cc0bd0ff45de
CriszMendoza <criszmendoza212@gmail.com>
2022-05-26 17:22:53 -0500

    Vuex: almacenar distancia y duración

--- src/store/map/actions.ts
+++ src/store/map/actions.ts
@@ -7,13 +7,17 @@ import { DirectionsResponse } from '../../interfaces/directions';
 export type LngLat = number[]
 
 const actions: ActionTree<MapState, StateInterface> = {
-	async getRouteBetweenPoints(_, {start, end}: {start:LngLat, end:LngLat}){
+	async getRouteBetweenPoints({ commit }, {start, end}: {start:LngLat, end:LngLat}){
 		
 		const resp = await directionApi.get<DirectionsResponse>(`/${start.join(',')};${end.join(',')}`)
 		if (resp.data.code === 'NoRoute') {
 			console.log([]);
 		}else{
-			console.log(resp.data.routes[0].geometry);
+			commit('setDistanceDuraton', {
+				distance: resp.data.routes[0].distance,
+				duration: resp.data.routes[0].duration
+			})
+			commit('setRoutesPolyline', resp.data.routes[0].geometry.coordinates)
 		}
 		
 	}
--- src/store/map/mutations.ts
+++ src/store/map/mutations.ts
@@ -36,6 +36,76 @@ const mutation: MutationTree<MapState> = {
 			state.markers.push(marker)
 			
 		}
+
+		if (state.map.getLayer('RouteString')) {
+			state.map.removeLayer('RouteString')
+			state.map.removeSource('RouteString')
+			state.distance = undefined
+			state.duration = undefined
+		}
+	},
+	setRoutesPolyline(state, coords: number[][]){		
+
+		const start = coords[0]
+		const end = coords[coords.length - 1]
+
+		// Definir los bounds
+
+		const bounds = new mapboxgl.LngLatBounds(
+			[start[0], start[1]],
+			[end[0], end[1]],
+		)
+
+		for (const coord of coords) {
+			const newCoord: [number, number] = [coord[0], coord[1]]
+			bounds.extend(newCoord)
+		}
+
+		state.map?.fitBounds(bounds,{ padding: 200})
+
+		const sourceData: mapboxgl.AnySourceData = {
+			type: 'geojson',
+			data:{
+				type: 'FeatureCollection',
+				features: [
+					{
+						type: 'Feature',
+						properties: {},
+						geometry: {
+							type: 'LineString',
+							coordinates: coords
+						}
+					}
+				]
+			}
+		}
+
+		if (state.map?.getLayer('RouteString')) {
+			state.map.removeLayer('RouteString')
+			state.map.removeSource('RouteString')
+		}
+
+		state.map?.addSource('RouteString', sourceData)
+
+		state.map?.addLayer({
+			id: 'RouteString',
+			type:'line',
+			source: 'RouteString',
+			layout:{
+				"line-cap":'round',
+				"line-join": 'round'
+			},
+			paint:{
+				'line-color': 'black',
+				'line-width': 3
+			}
+
+		})
+	},
+	setDistanceDuraton(state, {distance, duration}: {distance: number, duration: number}){
+
+		state.distance = Math.round((distance/1000) * 100) / 100
+		state.duration = Math.floor(duration/60)
 	}
 }
 
